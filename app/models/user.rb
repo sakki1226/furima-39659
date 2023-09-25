71049cb18ab81b8a0f838934403af8d057f9e8c7
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-09-25 18:54:56 +0900

    Userモデルの単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :last_name_kana,  presence: true
   validates :birthday,        presence: true
+
+  with_options presence: true do
+    validates :nickname, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@test'}
+    password              {'aaaa1111'}
+    password_confirmation {password}
+    first_name            {'山田'}
+    last_name             {'一郎'}
+    first_name_kana       {'ヤマダ'}
+    last_name_kana        {'イチロウ'}
+    birthday              {'19310101'}
+  end
+end
