631cec13335bc4071f2af1d4ab45d1a15970a0e4
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-09-30 18:33:57 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
-    @items = Item.all
     @items = Item.includes(:user).order("created_at DESC")
   end
 
@@ -20,7 +19,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
