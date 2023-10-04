66c5306ce77082b3b6044ff6789babcaac7f8ed1
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-10-04 18:43:58 +0900

    ログイン有無での動作切り替えの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
-  
+  before_action : , only: [:index]
+  before_action :move_to_index, only: [:index]
+
   
   def index
     @order_address = OrderAddress.new
@@ -22,4 +24,13 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street, :building, :phone, :item_id, :user_id).merge(user_id: current_user.id, item_id: @item.id)
   end
+
+  def move_to_index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id]) 
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
 end
