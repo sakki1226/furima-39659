5df74514c7b8400a4364d67c52cc28268f407d3d
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-10-04 18:18:59 +0900

    商品購入情報の保存機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
 class OrdersController < ApplicationController
   
+  
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id]) 
   end
 
   def create
+    @item = Item.find(params[:item_id]) 
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
@@ -17,7 +19,7 @@ class OrdersController < ApplicationController
 
   private
 
-  def order_address
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street, :building, :phone, :item_id, :user_id).merge(user_id: current_user.id)
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street, :building, :phone, :item_id, :user_id).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,16 +3,15 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone, :item_id, :user_id
 
   with_options presence: true do
-    validates :user_id
+    validates :city, :street, :item_id, :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone, format: { with: /\A\d{10,11}\z/, message: "電話番号は10桁以上11桁以内の半角数値である必要があります" }
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-  validates :prefecture_id, :city, :street, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
 
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, phone: phone, user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, phone: phone, order_id: order.id)
   end
 end
