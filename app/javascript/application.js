34ade2923ff14ddac77caf3f85514ce079e4e837
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-10-11 17:02:40 +0900

    application.jsの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./sales_fee_profit"
+import "sales_fee_profit"
 import "card"
