446a73481f4c1c9dd68b0f8b1f0bf2b7ad588c3b
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-10-12 12:27:31 +0900

    最終課題提出に伴う修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,5 +14,5 @@ class Item < ApplicationRecord
 
   validates :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,6 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
   with_options presence: true do
-    validates :nickname, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
